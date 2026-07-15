efa7a76b03fa43631be44a1ac7262ab5447ef30d
Sander Mooses <moosessander@gmail.com>
2026-07-15 22:58:49 +0300

    Added ssh_public_key and admin_username vars for VM authentication

---
+++ Labs/07-Security-Pipeline/terraform/variables.tf
@@ -0,0 +1,27 @@
+variable "prefix" {
+  description = "naming prefix for all resources"
+  type = string
+  default = "lab7"
+}
+
+variable "location" {
+    description = "Region to deploy into"
+    type = string
+    default = "westeurope"
+}
+
+variable "subscription.id" {
+  description = "AZ subscription ID"
+  type = string
+}
+
+variable "ssh_public_key" {
+  description = "SSH public key for the VM admin"
+  type = string
+}
+
+variable "admin_username" {
+  description = "Admin username for the VM"
+  type = string
+  default = "azureuser"
+}
