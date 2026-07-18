b32d077625e861a9b6e596ff991ae03876b4559e
Sander Mooses <moosessander@gmail.com>
2026-07-18 20:34:45 +0300

    Added remote state backend with az ad auth, fixed pipeline env vars

--- Labs/07-Security-Pipeline/terraform/main.tf
+++ Labs/07-Security-Pipeline/terraform/main.tf
@@ -1,5 +1,13 @@
 terraform {
   required_version = ">= 1.5.0"
+
+  backend "azurerm" {
+    resource_group_name = "terraform-state-rg"
+    storage_account_name = "sandertfstate"
+    container_name = "tfstate"
+    key = "lab07.terraform.tfstate"
+    use_azuread_auth = true
+  }
   required_providers {
     azurerm = {
         source = "hashicorp/azurerm"
