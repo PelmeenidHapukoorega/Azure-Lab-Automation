cdd235ff3d9cb6c322e842a4fda8d8752b20905c
Sander Mooses <moosessander@gmail.com>
2026-07-19 23:00:19 +0300

    Added public IP with static allocation

--- Labs/08-Linux-Administration/terraform/main.tf
+++ Labs/08-Linux-Administration/terraform/main.tf
@@ -37,3 +37,11 @@ module "networking" {
     { name = "default", address_prefixes = ["10.0.1.0/24"] }
   ]
 }
+
+resource "azurerm_public_ip" "this" {
+  name = "${var.prefix}-pip"
+  location = var.location
+  resource_group_name = azurerm_resource_group.this.name
+  allocation_method = "Static"
+  sku = "Standard"
+}
