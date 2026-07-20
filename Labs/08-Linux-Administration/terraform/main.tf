95034781f194509afeda17f38a8bbcc13e40e215
Sander Mooses <moosessander@gmail.com>
2026-07-20 21:48:13 +0300

    Added VM resource with Ubuntu lts image and set the storage account type to LRS

--- Labs/08-Linux-Administration/terraform/main.tf
+++ Labs/08-Linux-Administration/terraform/main.tf
@@ -58,3 +58,30 @@ resource "azurerm_network_interface" "this" {
     public_ip_address_id = azurerm_public_ip.this.id
   }
 }
+
+resource "azurerm_linux_virtual_machine" "this" {
+  name = "${var.prefix}-vm"
+  location = var.location
+  resource_group_name = azurerm_resource_group.this.name
+  size = "Standard_D2as_v6"
+  admin_username = var.admin_username
+
+  network_interface_ids = [azurerm_network_interface.this.id]
+
+  admin_ssh_key {
+    username = var.admin_username
+    public_key = var.ssh_public_key
+  }
+
+  os_disk {
+    caching = "ReadWrite"
+    storage_account_type = "StandardSSD_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer = "0001-com-ubuntu-server-jammy"
+    sku = "22_04-lts-gen2"
+    version = "latest"
+  }
+}
