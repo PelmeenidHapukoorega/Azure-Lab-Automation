43542613164eaf4c959f7976a8df24caa0855894
Sander Mooses <moosessander@gmail.com>
2026-07-21 17:27:08 +0300

    Added infra resources, rg, networking modulem public ip etc that i used in the previous lab, since its familiar code at this point and adds no additional value of writing this by hand again

--- Labs/09-Ansible/terraform/main.tf
+++ Labs/09-Ansible/terraform/main.tf
@@ -21,3 +21,77 @@ provider "azurerm" {
   features {}
   subscription_id = var.subscription_id
 }
+
+resource "azurerm_resource_group" "this" {
+  name = "${var.prefix}-rg"
+  location = var.location
+}
+
+module "networking" {
+  source = "git::https://github.com/PelmeenidHapukoorega/Deployment-templates.git//terraform/modules/networking?ref=9733a99"
+
+  prefix = var.prefix
+  location = var.location
+  resource_group_name = azurerm_resource_group.this.name
+  management_allowed_cidr = "194.150.65.93/32"
+  subnets = [
+    { name = "default", address_prefixes = ["10.0.1.0/24"] }
+  ]
+}
+
+resource "azurerm_public_ip" "this" {
+  name = "${var.prefix}-pip"
+  location = var.location
+  resource_group_name = azurerm_resource_group.this.name
+  allocation_method = "Static"
+  sku = "Standard"
+}
+
+resource "azurerm_network_interface" "this" {
+  name = "${var.prefix}-nic"
+  location = var.location
+  resource_group_name = azurerm_resource_group.this.name
+
+  ip_configuration {
+    name = "internal"
+    subnet_id = module.networking.subnet_ids["default"]
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.this.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "this" {
+  name = "${var.prefix}-vm"
+  location = var.location
+  resource_group_name = azurerm_resource_group.this.name
+  size = "Standard_D2as_v6"
+  admin_username = var.admin_username
+
+  network_interface_ids = [azurerm_network_interface.this.id]
+
+  admin_ssh_key {
+    username = var.admin_username
+    public_key = var.ssh_public_key
+  }
+
+  os_disk {
+    caching = "ReadWrite"
+    storage_account_type = "StandardSSD_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer = "0001-com-ubuntu-server-jammy"
+    sku = "22_04-lts-gen2"
+    version = "latest"
+  }
+}
+
+resource "azurerm_virtual_machine_extension" "azure_monitor" {
+  name = "${var.prefix}-ama"
+  virtual_machine_id = azurerm_linux_virtual_machine.this.id
+  publisher = "Microsoft.Azure.Monitor"
+  type = "AzureMonitorLinuxAgent"
+  type_handler_version = "1.0"
+  auto_upgrade_minor_version = true
+}
