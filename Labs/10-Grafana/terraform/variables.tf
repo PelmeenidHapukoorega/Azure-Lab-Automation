3a6931ce7aac33e58df509a586a10d092107f1d5
Sander Mooses <moosessander@gmail.com>
2026-07-26 00:23:24 +0300

    Removed ssh public key and admin username variables because no VM

--- Labs/10-Grafana/terraform/variables.tf
+++ Labs/10-Grafana/terraform/variables.tf
@@ -14,14 +14,3 @@ variable "subscription_id" {
   description = "AZ subscription ID"
   type = string
 }
-
-variable "ssh_public_key" {
-  description = "SSH public key for the VM admin"
-  type = string
-}
-
-variable "admin_username" {
-  description = "Admin username for the VM"
-  type = string
-  default = "azureuser"
-}
