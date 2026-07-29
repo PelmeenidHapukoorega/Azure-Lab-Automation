810420881a887ea18a7f613d1f89c6b31da243fb
Sander Mooses <moosessander@gmail.com>
2026-07-29 21:51:45 +0300

    terraform block and provider config for project 11

---
+++ Labs/11-Kubernetes-Networking/terraform/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  backend "azurerm" {
+    resource_group_name = "terraform-state-rg"
+    storage_account_name = "sandertfstate"
+    container_name = "tfstate"
+    key = "lab11.terraform.tfstate"
+    use_azuread_auth = true
+  }
+
+    required_providers {
+        azurerm = {
+            source = "hashicorp/azurerm"
+            version = "~> 4.0"
+        }
+    }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false 
+    }
+  }
+  subscription_id = var.subscription_id
+}
